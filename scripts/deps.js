87bdea682b51bd6c03de6430fa7b52618744daf3
execaman <151807496+execaman@users.noreply.github.com>
2025-11-11 23:01:25 +0530

    test: auto mod & dep sync

---
+++ scripts/deps.js
@@ -0,0 +1,11 @@
+import { readFileSync, writeFileSync } from "node:fs";
+
+const src = JSON.parse(readFileSync("package.json", "utf8"));
+const ejs = JSON.parse(readFileSync("./ejs/package.json", "utf8"));
+
+for (const dep in ejs.dependencies) {
+  if (dep in src.dependencies) continue;
+  src.dependencies[dep] = ejs.dependencies[dep];
+}
+
+writeFileSync("package.json", JSON.stringify(src, null, 2), "utf8");
