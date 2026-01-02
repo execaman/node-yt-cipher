badb939432cae19c1de04e60f7cc75bb80dbddb7
execaman <151807496+execaman@users.noreply.github.com>
2026-01-02 16:59:01 +0530

    refactor: deps synchronization

--- scripts/deps.js
+++ scripts/deps.js
@@ -3,8 +3,14 @@ import { readFileSync, writeFileSync } from "node:fs";
 const src = JSON.parse(readFileSync("package.json", "utf8"));
 const ejs = JSON.parse(readFileSync("./ejs/package.json", "utf8"));
 
+for (const dep in src.ejsDependencies) {
+  delete src.dependencies[dep];
+}
+
 for (const dep in ejs.dependencies) {
   src.dependencies[dep] = ejs.dependencies[dep];
 }
+
+src.ejsDependencies = ejs.dependencies;
 
 writeFileSync("package.json", JSON.stringify(src, null, 2) + "\n", "utf8");
